5f03687dc45ffcb764b0b90697b0465d58d5be82
Andrea Salati <andrea.salati@epfl.ch>
2022-12-12 14:46:51 +0100

    added noise sigma

--- DATA_generator.py
+++ DATA_generator.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def DataGenerator(N_Samples=100, N_PeriodicGenes=20, N_NonPeriodicGenes=0, noise=True, Means=False):
+def DataGenerator(N_Samples=100, N_PeriodicGenes=20, N_NonPeriodicGenes=0, noise=True, Means=False,NoiseSigma=1):
    
     phi = np.random.rand((N_Samples))*2*np.pi
     coeff=np.random.normal(0, 1, (N_PeriodicGenes,2) )
@@ -10,8 +10,12 @@ def DataGenerator(N_Samples=100, N_PeriodicGenes=20, N_NonPeriodicGenes=0, noise
         NP=np.zeros((N_NonPeriodicGenes, N_Samples))
         E=np.vstack([E,NP])
   
+    if Means:
+        means = np.random.uniform( size=(N_PeriodicGenes+N_NonPeriodicGenes,1) )
+        E=E+means
+
     if noise:
-        noise_matrix=np.random.normal( size=(N_PeriodicGenes+N_NonPeriodicGenes,N_Samples) )
+        noise_matrix=np.random.normal(0,NoiseSigma, size=(N_PeriodicGenes+N_NonPeriodicGenes,N_Samples) )
         E=E+noise_matrix
 
     return E,phi,coeff
@@ -31,3 +35,5 @@ def NonUniformDataGenerator(N_Samples=100, N_PeriodicGenes=20, N_NonPeriodicGene
         E=E+noise_matrix
 
     return E,phi,coeff
+
+
